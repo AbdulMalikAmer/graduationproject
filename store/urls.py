55c8ee2ab124d30148c161fc7398df258bd1f64a
MalikAmer <mcmalek12@gmail.com>
2024-12-19 00:20:30 +0200

    Initial commit with modified project

---
+++ store/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views
+from .views import add_product, edit_product
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+    path('update_password/', views.update_password, name='update_password'),
+    path('update_user/', views.update_user, name='update_user'),
+    path('update_info/', views.update_info, name='update_info'),
+    path('product/<int:pk>', views.product, name='product'),
+    path('category/<str:foo>', views.category, name='category'),
+    path('category_summary/', views.category_summary, name='category_summary'),
+    path('search/', views.search, name='search'),
+    path('product/new/', add_product, name='add_product'),  # صفحة إضافة منتج جديد
+    path('product/<int:product_id>/edit/', edit_product, name='edit_product'),  # تعديل المنتج
+    
+]
